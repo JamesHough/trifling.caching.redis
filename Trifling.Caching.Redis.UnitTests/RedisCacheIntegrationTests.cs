374f2229c212f04bcfe8b9b65db18656015465d3
James Hough <James Hough>
2017-01-28 21:39:24 +0200

    Implementation of Redis caching

---
+++ Trifling.Caching.Redis.UnitTests/RedisCacheIntegrationTests.cs
@@ -0,0 +1,88 @@
+﻿namespace Trifling.Caching.Redis
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RedisCacheIntegrationTests
+    {
+
+        [TestMethod]
+        public void RedisCacheIntegrationTests_Cache_WhenValueCached_ThenSameValueRetrieved()
+        {
+            // ----- Arrange -----
+            var data = new byte[] { 59, 58, 57, 56, 55, 54, 53, 52, 51, 90, 89, 88, 87, 86, 85 };
+            var cacheKey = "15bytes-cache-retrieve";
+
+            var redis = new RedisEngine();
+            redis.Initialise(null);
+
+            // ----- Act -----
+            redis.Cache(cacheKey, data, TimeSpan.FromSeconds(5d));
+
+            var retrieved = redis.Retrieve(cacheKey);
+
+            // ----- Asset -----
+            Assert.IsNotNull(retrieved);
+            Assert.AreEqual(data.Length, retrieved.Length);
+            Assert.AreNotSame(data, retrieved, "the engine cheated and returned the exact same instance.");
+            for (var i = 0; i < data.Length; i++)
+            {
+                Assert.AreEqual(data[i], retrieved[i], "expected array values did not match at " + i);
+            }
+        }
+        
+        [TestMethod]
+        public void RedisCacheIntegrationTests_Remove_WhenRemoveExistingCacheValue_ThenReturnsTrue()
+        {
+            // ----- Arrange -----
+            var data = new byte[] { 61, 71, 81, 91, 101, 111, 121, 131, 141, 151, 161, 171, 181, 191, 120, 121, 122, 123, 124 };
+            var cacheKey = "19bytes-cache-remove";
+
+            var redis = new RedisEngine();
+            redis.Initialise(null);
+
+            // ----- Act -----
+            redis.Cache(cacheKey, data, TimeSpan.FromSeconds(25d));
+
+            var removed = redis.Remove(cacheKey);
+
+            // ----- Asset -----
+            Assert.IsTrue(removed);
+        }
+
+        [TestMethod]
+        public void RedisCacheIntegrationTests_Remove_WhenRemoveNonExistentCacheKey_ThenReturnsFalse()
+        {
+            // ----- Arrange -----
+            var cacheKey = "neverexisted-remove";
+
+            var redis = new RedisEngine();
+            redis.Initialise(null);
+
+            // ----- Act -----
+            var removed = redis.Remove(cacheKey);
+
+            // ----- Asset -----
+            Assert.IsFalse(removed);
+        }
+
+        [TestMethod]
+        public void RedisCacheIntegrationTests_Retrieve_WhenNonExistentCacheKey_ThenReturnsNull()
+        {
+            // ----- Arrange -----
+            var cacheKey = "neverexisted-retrieve";
+
+            var redis = new RedisEngine();
+            redis.Initialise(null);
+
+            // ----- Act -----
+            var retrieved = redis.Retrieve(cacheKey);
+
+            // ----- Asset -----
+            Assert.IsNull(retrieved);
+        }
+
+    }
+}
---
+++ Trifling.Caching.Redis/RedisEngine.cs
@@ -0,0 +1,219 @@
+﻿namespace Trifling.Caching.Redis
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Text;
+
+    using Microsoft.Extensions.Logging;
+
+    using StackExchange.Redis;
+
+    using Trifling.Caching.Interfaces;
+
+    public class RedisEngine : ICacheEngine
+    {
+        /// <summary>
+        /// A value indicating if the Initialize method has already been called.
+        /// </summary>
+        private bool _initialized = false;
+
+        /// <summary>
+        /// The open connection to Redis server.
+        /// </summary>
+        private static Lazy<ConnectionMultiplexer> RedisConnection;
+
+        /// <summary>
+        /// The given configuration options for connecting to Redis.
+        /// </summary>
+        private CacheEngineConfiguration _cacheEngineConfiguration;
+
+        /// <summary>
+        /// The given logger that will be used by Redis server to report on events.
+        /// </summary>
+        private readonly ILogger _cacheEventLogger;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="RedisEngine"/> class.  
+        /// </summary>
+        public RedisEngine()
+            : this(null)
+        {
+        }
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="RedisEngine"/> class with the given
+        /// event logger for Redis to write to.
+        /// </summary>
+        /// <param name="cacheEventLogger">A logger which can accept logging information from Redis server.</param>
+        public RedisEngine(
+            ILogger cacheEventLogger)
+        {
+            this._cacheEventLogger = cacheEventLogger;
+        }
+
+        /// <summary>
+        /// Initialises the connection to Redis with the given configuration.
+        /// </summary>
+        /// <param name="cacheEngineConfiguration">The configuration options for connecting to Redis.</param>
+        public void Initialise(CacheEngineConfiguration cacheEngineConfiguration)
+        {
+            // if the Initialise method was already called, then any existing connection will need to be closed
+            // before reconfiguring with the new configuration.
+            if (this._initialized && RedisConnection.IsValueCreated)
+            {
+                RedisConnection.Value.Close();
+            }
+
+            // if the configuration is null then use default values (local Redis server is assumed).
+            this._cacheEngineConfiguration =
+                cacheEngineConfiguration ?? new CacheEngineConfiguration { Server = "localhost", Port = 6379 };
+            
+            // set-up the lazy loader which will make the connection when needed.
+            RedisConnection = new Lazy<ConnectionMultiplexer>(
+                () =>
+                {
+                    var c = ConnectionMultiplexer.Connect(this.GetRedisConfiguration(), this.GetLogEntryTextWriter());
+
+                    // add extra logging of Redis events.
+                    // TODO
+                    ////c.ErrorMessage += RedisConnection_ErrorMessage;
+                    ////c.ConnectionFailed += RedisConnection_ConnectionFailed;
+                    return c;
+                });
+
+            this._initialized = true;
+        }
+
+        public void Cache(string cacheEntryKey, byte[] value, TimeSpan expiry)
+        {
+            var db = this.GetDatabase();
+
+            db.StringSet(cacheEntryKey, value, expiry);
+        }
+
+        public bool Remove(string cacheEntryKey)
+        {
+            var db = this.GetDatabase();
+
+            return db.KeyDelete(cacheEntryKey);
+        }
+
+        public byte[] Retrieve(string cacheEntryKey)
+        {
+            var db = this.GetDatabase();
+
+            return db.StringGet(cacheEntryKey);
+        }
+
+        /// <summary>
+        /// Gets the active Redis database (usually 0, depending on the configuration this can be anything up to 16).
+        /// </summary>
+        /// <returns>Returns a database connection for performing Redis server calls.</returns>
+        private IDatabase GetDatabase()
+        {
+            return RedisConnection.Value.GetDatabase();
+        }
+
+        /// <summary>
+        /// Builds a Redis <see cref="ConfigurationOptions"/> object from the current instance's
+        /// <see cref="_cacheEngineConfiguration"/>. 
+        /// </summary>
+        /// <returns>Returns a <see cref="ConfigurationOptions"/> instance for connecting to Redis.</returns>
+        private ConfigurationOptions GetRedisConfiguration()
+        {
+            // fix bad server names
+            if (string.IsNullOrWhiteSpace(this._cacheEngineConfiguration.Server))
+            {
+                // assume the server is local
+                this._cacheEngineConfiguration.Server = "localhost";
+            }
+
+            // fix any invalid port numbers
+            if (this._cacheEngineConfiguration.Port <= 0 || this._cacheEngineConfiguration.Port > 65535)
+            {
+                // reset this bad port to the default for Redis Server.
+                this._cacheEngineConfiguration.Port = 6379;
+            }
+
+            var connection = new StringBuilder();
+            connection.AppendFormat(CultureInfo.InvariantCulture, "{0}:{1}", this._cacheEngineConfiguration.Server, this._cacheEngineConfiguration.Port);
+
+            // go through the list of alternative servers and remove bad entries.
+            if (this._cacheEngineConfiguration.AlternativeServers != null)
+            {
+                foreach (var server in this._cacheEngineConfiguration.AlternativeServers.Keys)
+                {
+                    if (string.IsNullOrWhiteSpace(server))
+                    {
+                        // skip bad server names
+                        continue;
+                    }
+
+                    if (this._cacheEngineConfiguration.AlternativeServers[server] <= 0 
+                        || this._cacheEngineConfiguration.AlternativeServers[server] > 65535)
+                    {
+                        // reset this bad port to the default for Redis Server.
+                        this._cacheEngineConfiguration.AlternativeServers[server] = 6379;
+                    }
+
+                    connection.AppendFormat(
+                        CultureInfo.InvariantCulture,
+                        ",{0}:{1}",
+                        server,
+                        this._cacheEngineConfiguration.AlternativeServers[server]);
+                }
+            }
+
+            // if the connection timeout was specified then add that to the connection options.
+            if (this._cacheEngineConfiguration.ConnectionTimeoutMilliseconds > 0
+                && this._cacheEngineConfiguration.ConnectionTimeoutMilliseconds <= 20000)
+            {
+                connection.AppendFormat(CultureInfo.InvariantCulture, ",connectTimeout={0}", this._cacheEngineConfiguration.ConnectionTimeoutMilliseconds);
+            }
+
+            // if the request timeout was specified then add that to the conection options.
+            if (this._cacheEngineConfiguration.ResponseTimeoutMilliseconds > 0
+                && this._cacheEngineConfiguration.ResponseTimeoutMilliseconds <= 20000)
+            {
+                connection.AppendFormat(CultureInfo.InvariantCulture, ",syncTimeout={0}", this._cacheEngineConfiguration.ResponseTimeoutMilliseconds);
+            }
+
+            // if there are additional parameter options then append them as-is.
+            if (!string.IsNullOrWhiteSpace(this._cacheEngineConfiguration.AdditionalConnectionOptions))
+            {
+                if (!this._cacheEngineConfiguration.AdditionalConnectionOptions.StartsWith(","))
+                {
+                    connection.Append(',');
+                }
+
+                connection.Append(this._cacheEngineConfiguration.AdditionalConnectionOptions);
+            }
+
+            // check if "abortConnect" was explicitly set.  If not then set it to false for more stability in single
+            // connection, long lifetime setups.
+            var connectionString = connection.ToString();
+            if (!connectionString.Contains("abortConnect="))
+            {
+                connection.Append(",abortConnect=false");
+                connectionString = connection.ToString();
+            }
+
+            return ConfigurationOptions.Parse(connectionString);
+        }
+
+        /// <summary>
+        /// Wraps the given <see cref="_cacheEventLogger"/> in a Text Writer (provided by 
+        /// <see cref="Trifling.Logging.LoggingTextWriter"/> for Redis to use as a logging destination.
+        /// </summary>
+        /// <returns>Returns an instance of a <see cref="TextWriter"/> for logging. If there is no 
+        /// logger available, then returns null.
+        /// </returns>
+        private TextWriter GetLogEntryTextWriter()
+        {
+            return (this._cacheEventLogger == null)
+                ? null
+                : new Trifling.Logging.LoggingTextWriter(this._cacheEventLogger, LogLevel.Trace);
+        }
+    }
+}
